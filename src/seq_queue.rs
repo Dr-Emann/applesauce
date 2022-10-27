26ef02fb59bbd6afc6664b6c58ba74bf9e35a826
Zachary Dremann <dremann@gmail.com>
2022-10-27 16:23:39 -0400

    Working sequential queue

--- src/seq_queue.rs
+++ src/seq_queue.rs
@@ -1,31 +1,142 @@
-use crossbeam_channel::{Receiver, Sender};
-use std::sync::Arc;
+use std::fmt;
+use std::fmt::Formatter;
 
-pub struct SeqQueue<T> {
-    tx: Sender<Receiver<T>>,
-    rx: Receiver<Receiver<T>>,
+#[derive(Debug)]
+pub struct Sender<T>(crossbeam_channel::Sender<crossbeam_channel::Receiver<T>>);
+
+#[derive(Debug)]
+pub struct Receiver<T>(crossbeam_channel::Receiver<crossbeam_channel::Receiver<T>>);
+
+pub struct Slot<T>(crossbeam_channel::Sender<T>);
+
+pub fn bounded<T>(cap: usize) -> (Sender<T>, Receiver<T>) {
+    let (tx, rx) = crossbeam_channel::bounded(cap);
+    (Sender(tx), Receiver(rx))
 }
 
-impl<T> SeqQueue<T> {
-    pub fn new() -> Self {
-        let (tx, rx) = crossbeam_channel::bounded(8);
-        Self { tx, rx }
-    }
-
-    pub fn reserve_slot(&self) -> Slot<T> {
+impl<T> Sender<T> {
+    pub fn prepare_send(&self) -> Option<Slot<T>> {
         let (tx, rx) = crossbeam_channel::bounded(1);
-
-        self.tx.send(rx).unwrap();
-        Slot { tx }
-    }
-
-    // TODO: Result
-    pub fn next(&self) -> Option<T> {
-        let inner = self.rx.recv().ok()?;
-        inner.recv().ok()
+        self.0.send(rx).ok()?;
+        Some(Slot(tx))
     }
 }
 
-pub struct Slot<T> {
-    tx: Sender<T>,
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<T> Slot<T> {
+    pub fn finish(self, item: T) -> Result<(), crossbeam_channel::SendError<T>> {
+        self.0.send(item)
+    }
+}
+
+impl<T> Receiver<T> {
+    pub fn recv(&self) -> Result<T, RecvError> {
+        let inner_chan = self.0.recv().map_err(|_| RecvError::Finished)?;
+        inner_chan.recv().map_err(|_| RecvError::ItemRecvError)
+    }
+}
+
+pub struct Iter<T> {
+    receiver: Receiver<T>,
+}
+
+impl<T> IntoIterator for Receiver<T> {
+    type Item = T;
+    type IntoIter = Iter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter { receiver: self }
+    }
+}
+
+impl<T> Iterator for Iter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.receiver.recv().ok()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct SendError;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum RecvError {
+    Finished,
+    ItemRecvError,
+}
+
+impl RecvError {
+    const fn message(self) -> &'static str {
+        match self {
+            RecvError::Finished => "receiving on an empty and disconnected channel",
+            RecvError::ItemRecvError => "item in sequential queue was dropped without completion",
+        }
+    }
+}
+
+impl fmt::Display for RecvError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.write_str(self.message())
+    }
+}
+
+impl std::error::Error for RecvError {}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use super::*;
+
+    #[test]
+    fn order_after_sending() {
+        let (tx, rx) = bounded(2);
+
+        let first = tx.prepare_send().unwrap();
+        assert_eq!(rx.0.len(), 1);
+        let second = tx.prepare_send().unwrap();
+        assert_eq!(rx.0.len(), 2);
+
+        drop(tx);
+
+        second.finish(2).unwrap();
+        first.finish(1).unwrap();
+
+        assert_eq!(rx.0.len(), 2);
+
+        assert_eq!(rx.recv().unwrap(), 1);
+        assert_eq!(rx.recv().unwrap(), 2);
+        assert!(rx.recv().is_err());
+    }
+
+    #[test]
+    fn across_threads() {
+        let (tx, rx) = bounded(2);
+
+        let sender_handle = std::thread::spawn(move || {
+            let tx = tx;
+            for i in 0..1000 {
+                let slot = tx.prepare_send().unwrap();
+                std::thread::spawn(move || {
+                    // slow down some finishes
+                    if i % 3 == 0 {
+                        std::thread::sleep(Duration::from_micros(10));
+                    }
+                    slot.finish(i).unwrap();
+                });
+            }
+        });
+
+        for i in 0..1000 {
+            assert_eq!(rx.recv().unwrap(), i);
+        }
+        assert_eq!(rx.recv().unwrap_err(), RecvError::Finished);
+
+        sender_handle.join().unwrap();
+    }
 }
