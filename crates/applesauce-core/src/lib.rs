b771994ebabf88fd7e5de3ab7103530fcab73209
Zachary Dremann <dremann@gmail.com>
2024-12-16 22:58:46 -0500

    chore: fix clippy warnings

--- crates/applesauce-core/src/lib.rs
+++ crates/applesauce-core/src/lib.rs
@@ -12,7 +12,7 @@ pub const BLOCK_SIZE: usize = 0x10000;
 #[must_use]
 #[inline]
 pub const fn num_blocks(size: u64) -> u64 {
-    (size + (BLOCK_SIZE as u64 - 1)) / (BLOCK_SIZE as u64)
+    size.div_ceil(BLOCK_SIZE as u64)
 }
 
 /// Rounds `size` up to the nearest multiple of `block_size`.
